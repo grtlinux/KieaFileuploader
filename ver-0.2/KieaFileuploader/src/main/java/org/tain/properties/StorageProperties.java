0160e8837adc61435438b2124c7faf8a066dd6ff
grtlinux <grtlinux@gmail.com>
2020-12-28 23:40:33 +0900

    to FILES

--- ver-0.2/KieaFileuploader/src/main/java/org/tain/properties/StorageProperties.java
+++ ver-0.2/KieaFileuploader/src/main/java/org/tain/properties/StorageProperties.java
@@ -7,7 +7,8 @@ public class StorageProperties {
 
 	//rivate String location = "upload-dir"; // Users/kangmac/STS_workspace/KieaDemo-3/upload-dir
 	//private String location = "/Users/kangmac/upload-dir";
-	private String location = System.getenv("HOME") + "/upload-dir";
+	//private String location = System.getenv("HOME") + "/upload-dir";
+	private String location = System.getenv("HOME") + "/FILES";
 	
 	public String getLocation() {
 		return this.location;
